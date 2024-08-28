f3dbd3bb999ace2668564fe1a586412209c94964
MatiasCordich <m.scordich@gmail.com>
2024-08-27 23:43:40 -0300

    Controlador: Comentarios agregados

--- LogisticaContainers.Web/Controllers/HomeController.cs
+++ LogisticaContainers.Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Container = LogisticaContainers.Managers.Entidades.Container;
 
 namespace LogisticaContainers.Web.Controllers
 {
+    // ------- Controlador de Home: Practicamente va a contner los metodos que van a funcionar como intermediarios entre los modelos y las vistas ------- //
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -16,11 +17,21 @@ namespace LogisticaContainers.Web.Controllers
             _logger = logger;
         }
 
+        // ------- Cada método del controlador debe tener el nombre de su vista correspondiente ------- //
         public IActionResult Index()
         {
-            
+            // ------- En la vista Index vamos a crear el Container y plasmar sus datos en la vista ------- //
+
+            // Manager: Creamos el manager de Container que tiene la logica de negocio
             ContainerManager manager = new ContainerManager();
+
+            // Container: Creamos el container mediane el método del manager CrearContainer()
+            // Nos devuelve el objeto Container creado y lo guardamos en una variable del mismo tipo
             Container container = manager.CrearContainer();
+
+            // ViewBag: Podemos intercambiar datos entre el controlador y la vista sin necesidad de definir un tipo de datos específico
+            // dataContainer es el nombre que le damos nostoros
+            // Recibira como valor el objeto container
             ViewBag.dataContainer = container;
             return View();
         }
