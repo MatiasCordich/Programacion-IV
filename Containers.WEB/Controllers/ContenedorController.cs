15c3db4adb6ecca461707411b4121c2fede66ad0
MatiasCordich <m.scordich@gmail.com>
2024-09-03 22:26:42 -0300

    Clase 3: Controller logica terminado

--- Containers.WEB/Controllers/ContenedorController.cs
+++ Containers.WEB/Controllers/ContenedorController.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Containers.WEB.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Containers.WEB.Controllers
 {
+    /*
+     Este es nuestro CONTROLLER de Contenedoa. 
+     Aca estaran todas los metodos del controler que nos permitiran gestionar el MODELO de Contenedor
+     y la vista de Contenedor.
+     */
+   
+    /*
+     Cada metodo tiene comentado dos cosas
+      - Metodo HTTP: Son metodos para hacer peticiones (GET, POST)
+      - Ruta: La ruta a la que funciona dicho metodo del controlador  
+     */
+
+    
     public class ContenedorController : Controller
     {
+
+        // Lista: Creamos una lista de contenedores en base a nuestro modelo de Contenedor creado. 
+        private List<ContenedorVM> _contenedores { get; set; }
+
+        // Constructor: Creamos el constructor del Controller
+        public ContenedorController() { 
+
+            // Instanciamos una lista de contenedores
+            _contenedores = new List<ContenedorVM>();
+
+            // Agregamos dos objetos de tipo ContenedorVM de nuestro modelo
+            _contenedores.Add(new ContenedorVM
+            {
+                IdContainer = 1, 
+                FechaAlta = DateTime.Now,
+                Identificacion = "ASDA-01",
+                Ubicacion = "Avenida Siempreviva 2792"
+            });
+
+            _contenedores.Add(new ContenedorVM
+            {
+                IdContainer = 2,
+                FechaAlta = DateTime.Now,
+                Identificacion = "ASDAASFASD-22343",
+                Ubicacion = "Calle Falsa 123"
+            });
+        }
+
         // GET: ContenedorController
         public ActionResult Index()
         {
-            return View();
+            // Retornamos en el Index la lista de contenedores
+            return View(_contenedores);
         }
 
         // GET: ContenedorController/Details/5
