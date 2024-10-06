3780b4d925f193ca1bb8a887fd9b40ce475b5ca6
MatiasCordich <m.scordich@gmail.com>
2024-10-06 16:31:06 -0300

    Se creo la entidad EstadoContainer

---
+++ UNLZ.LogisticaContainers/LogisticaContainers.Managers/Entidades/EstadoContainer.cs
@@ -0,0 +1,16 @@
+﻿using LogisticaContainers.Managers.Entidades.Auditoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticaContainers.Managers.Entidades
+{
+    public class EstadoContainer: Audit
+    {
+      public int IdEstadoContainer { get; set; }
+      public string Descripcion { get; set; }
+        
+    }
+}
