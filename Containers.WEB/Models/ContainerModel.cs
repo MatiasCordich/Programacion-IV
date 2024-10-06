3498bc5eee0d56964c7e4b16ea4c72ac5e532c77
MatiasCordich <m.scordich@gmail.com>
2024-10-06 16:21:35 -0300

    Se agrego un nuevo MODELO para Container

---
+++ Containers.WEB/Models/ContainerModel.cs
@@ -0,0 +1,11 @@
+﻿using LogisticaContainers.Managers.Entidades;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Containers.WEB.Models
+{
+    public class ContainerModel
+    {
+        public Container model { get; set; }
+        public List<SelectListItem> ListaEstadosItem { get; set; }
+    }
+}
