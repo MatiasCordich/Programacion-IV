260206a20c249077d4f25e26b1a2a5261ec64774
MatiasCordich <m.scordich@gmail.com>
2024-10-06 16:20:19 -0300

    Se agregó el Model Factory para Container

---
+++ UNLZ.LogisticaContainers/LogisticaContainers.Managers/ModelFactories/ContainerCompleto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticaContainers.Managers.ModelFactories
+{
+    public class ContainerCompleto
+    {
+        public int IdContainer { get; set; }
+        public string DescripcionContainer { get; set; }
+        public int IdEstadoContainer { get; set; }
+        public string Estado { get; set; }
+    }
+}
