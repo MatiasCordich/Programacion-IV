7294a833d01f14c93efce9f52533902e73d1936e
MatiasCordich <m.scordich@gmail.com>
2024-10-07 23:34:44 -0300

    Se agergo la etiqueta Authorize

--- Containers.WEB/Controllers/HomeController.cs
+++ Containers.WEB/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 using Containers.WEB.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Containers.WEB.Controllers
 {
+    
+    [Authorize] // --- Se agrego la etiqueta Authorize --- //
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
